616c65b277d9816cea4f6ff006cfacb398bfc343
Kathleen Geise <kmgeise2@gmail.com>
2023-01-29 20:38:25 -0700

    Add project files.

---
+++ IC01_kmg_SportsPro/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using IC01_kmg_SportsPro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IC01_kmg_SportsPro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ IC01_kmg_SportsPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IC01_kmg_SportsPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
